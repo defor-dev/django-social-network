801f60606f9fea270cd0d8b3fedd656ca6d7685f
defor <deforworks@gmail.com>
2019-12-31 18:05:23 +0300

    Password reset

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from .views import dashboard
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', dashboard, name='dashboard'),
@@ -12,4 +13,16 @@ urlpatterns = [
     path('password_change/done/',   
           auth_views.PasswordChangeDoneView.as_view(),
           name='password_change_done'),
+    path('password_reset/',
+          auth_views.PasswordResetView.as_view(),
+          name='password_reset'),
+    path('password_reset/done/',
+          auth_views.PasswordResetDoneView.as_view(),
+          name='password_reset_done'),
+    path('reset/<uib64>/<token>/',
+          auth_views.PasswordResetConfirmView.as_view(),
+          name='password_reset_confirm'),
+    path('reset/done/',
+          auth_views.PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'),
 ]
